5d8e87c216f44a03e47322ef7895aef06c514820
BelikovDenis <DenisBelikov1992@yandex.ru>
2025-07-13 10:14:35 +0300

    pytestpytest

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,8 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['infrasprint1main.ddns.net', 'localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://infrasprint1main.ddns.net:8443']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', ['*'])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
